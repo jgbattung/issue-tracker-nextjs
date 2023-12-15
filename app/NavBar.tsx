ca17c255f33005990db0ed27090ecf7398306103
Jireh Battung <jirehgbattung@gmail.com>
2023-12-15 12:46:59 +0800

    add navbar link styles

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,14 @@
+"use client"
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
-
+import classnames from 'classnames';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' }
@@ -14,7 +19,16 @@ const NavBar = () => {
       <Link href="/"><AiFillBug /></Link>
       <ul className='flex space-x-6'>
         {links.map((link) => (
-          <Link key={link.href} href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>
+          <Link 
+            key={link.href}
+            href={link.href}
+            className={classnames({
+              'text-zinc-900 font-medium': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover: text-zinc-800 transition-colors': true,
+            })}>
+              {link.label}
+          </Link>
         ))}
       </ul>
     </nav>
